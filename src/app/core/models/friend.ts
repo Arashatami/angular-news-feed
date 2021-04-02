7590ea2cf2cf2ea2050cb9ca65f275574cd140fb
Arash Hatami <arash.hatami738@gmail.com>
2021-04-02 22:59:09 +0430

    friends service

---
+++ src/app/core/models/friend.ts
@@ -0,0 +1,4 @@
+export interface Friend {
+  userId: number;
+  name: string;
+}
---
+++ src/app/core/models/news-feed.ts
@@ -0,0 +1,6 @@
+export interface NewsFeed {
+  id: number;
+  userId: number;
+  type: number;
+  content: string[];
+}
