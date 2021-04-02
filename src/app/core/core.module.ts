72fa57fbf30401b98c945438c0f8911829185108
Arash Hatami <arash.hatami738@gmail.com>
2021-04-02 19:43:05 +0430

    header component init

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
